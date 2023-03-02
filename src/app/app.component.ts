9652dc9c818460edc12303eabcc494713f77d4bc
paul-c-guest <ballsies@gmail.com>
2023-03-03 08:33:58 +1300

    working basic model [without hydration values; minimal css]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Flour } from './flour';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'bread-calc';
-}
+
+export class AppComponent implements OnInit {
+  title = 'Dough Hydro Calc';
+  flours: Flour[] = [];
+
+  ngOnInit(): void {
+    this.flours = [
+      { name: 'White Flour', amount: 350 },
+      { name: 'Spelt Flour', amount: 150 },
+    ]
+  }
+
+  add(name: string, amount: number): void {
+    if (name && amount) {
+      this.flours.push({ name, amount } as Flour);
+    }
+  }
+
+  remove(name: string) : void {
+    this.flours = this.flours.filter(flour => flour.name != name);
+  }
+
+  total(): number {
+    return this.flours.reduce((total, flour) => total + flour.amount, 0);
+  }
+ }
---
+++ src/app/flour.ts
@@ -0,0 +1,5 @@
+export interface Flour {
+  name: string;
+  amount: number;
+  hydration?: number;
+}
